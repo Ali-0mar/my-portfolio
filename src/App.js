54e728220fdb5699401889dc217924cc76bfa5b3
Ali-0mar <ali.o@storexweb.com>
2022-12-29 22:40:47 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -8,16 +8,14 @@ import Experience from './components/Experience'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path='/experience' element={<Experience />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path='/experience' element={<Experience />} />
+          </Route>
+        </Routes>
   )
 }
 
